f2cc96d5d31b8ef35597344ea048654f8934fa44
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-06-19 23:07:57 -0700

    Create inorderTraversal.java
    Solution to Inorder Traversal - LeetCode #94

---
+++ Medium Problems/inorderTraversal.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        ArrayList<Integer> soln = new ArrayList<>();
+        while(!stack.isEmpty() || root != null) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            soln.add(root.val);
+            root = root.right;
+        }
+        return soln;
+    }
+}
