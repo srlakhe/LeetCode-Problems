df6b9a4aea4ad31798d0716282800f05e28bdf77
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-06-19 23:13:14 -0700

    Create productExceptSelf.java
    Solution to Product of Array Except itself - LeetCode #238

---
+++ Medium Problems/productExceptSelf.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        int prod = 1;
+        for(int i = 0; i < nums.length; i++) {
+            result[i] = prod;
+            prod *= nums[i];
+        }
+        
+        prod = 1;
+        for(int i = nums.length-1; i >= 0; i--) {
+            result[i] *= prod;
+            prod *= nums[i];
+        }
+        return result;
+    }
+}
