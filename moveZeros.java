8e3152bc4d8cc69b94026d711772e5283ba16e07
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-06-19 19:47:22 -0700

    Create moveZeros.java
    Solution to move zeros - LeetCode #283

---
+++ moveZeros.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int insertIndex = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0)
+                continue;
+            nums[insertIndex++] = nums[i];
+        }
+        for(int i = insertIndex; i < nums.length; i++)
+            nums[i] = 0;
+    }
+}
