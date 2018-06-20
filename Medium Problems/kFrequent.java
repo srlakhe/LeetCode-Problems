af1913a31cadc91fad7b279e2dfadb71617834d6
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-06-19 23:57:12 -0700

    Create kFrequent.java
    Solution to Top k Frequent elements - LeetCode #347

---
+++ Medium Problems/kFrequent.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        ArrayList<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            hmap.put(nums[i], hmap.getOrDefault(nums[i], 0)+1);
+        }
+        
+        List<Integer>[] sortedMap = new List[nums.length+1];
+        for(int num : hmap.keySet()) {
+            int freq = hmap.get(num);
+            if(sortedMap[freq] == null)
+                sortedMap[freq] = new LinkedList<>();
+            sortedMap[freq].add(num);
+        }
+        
+        for(int i = sortedMap.length-1; i > 0 && k > 0; i--) {
+            if(sortedMap[i] != null) {
+                result.addAll(sortedMap[i]);
+                k -= sortedMap[i].size();
+                
+            }
+        }
+        return result;   
+    }
+}
