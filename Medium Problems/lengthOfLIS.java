173930996be0bb58ded73f82c346e0ac3486d656
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-06-29 18:48:35 +0530

    Create lengthOfLIS.java
    Length of Longest Increasing Subsequence - Leetcode #300

---
+++ Medium Problems/lengthOfLIS.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+       
+        for(int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for(int j = 0; j < i; j++)
+                if(nums[i] > nums[j])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+        return dp[dp.length-1];
+    }
+}
