e15da82e7a9cdffce64a915f1ee82efb68e334bd
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-06-29 17:04:59 +0530

    Create increasingTriplet.java
    Solution to increasing Triplet Subsequence - LeetCode #334

---
+++ Medium Problems/increasingTriplet.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+ 
+        int one = Integer.MAX_VALUE;
+        int two = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] <= one)
+                one = nums[i];
+            else if(nums[i] <= two)
+                two = nums[i];
+            else
+                return true;
+        }
+        return false;
+        
+    }
+}
