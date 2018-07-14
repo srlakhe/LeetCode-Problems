b4d31b056d62bf5caa85b2cf7ceff1660f3fac07
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-07-14 10:32:15 +0530

    Update longestIncreasingPath.java

--- Hard Problems/longestIncreasingPath.java
+++ Hard Problems/longestIncreasingPath.java
@@ -1,5 +1,7 @@
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
+        if(matrix.length == 0)
+            return 0;
         int[][] dp = new int[matrix.length][matrix[0].length];
         int max = 0;
         for(int i = 0; i < matrix.length; i++) {
@@ -12,13 +14,12 @@ class Solution {
         return max;
     }
     int calculatePath(int[][] matrix, int[][] dp, int i, int j) {
-      
         if(dp[i][j] > 0)
             return dp[i][j];
         int maxlen = 1;
         if(i+1 < matrix.length && matrix[i][j] < matrix[i+1][j])
             maxlen = Math.max(maxlen, 1 + calculatePath(matrix, dp, i+1, j));
-        if(j+1 < matrix.length && matrix[i][j] < matrix[i][j+1])
+        if(j+1 < matrix[0].length && matrix[i][j] < matrix[i][j+1])
            maxlen = Math.max(maxlen, 1 + calculatePath(matrix, dp, i, j+1));
         if(i-1 >= 0 && matrix[i][j] < matrix[i-1][j])
             maxlen = Math.max(maxlen, 1 + calculatePath(matrix, dp, i-1, j));
