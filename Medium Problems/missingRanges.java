bc266eb5199af6bd1ff61e58ddd03a6cf9d90fdb
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-07-25 19:17:36 +0530

    Create missingRanges.java
    Solution to Missing Ranges - Leetcode #163

---
+++ Medium Problems/missingRanges.java
@@ -0,0 +1,27 @@
+class Solution {
+  public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+      int next = lower;
+      List<String> result = new ArrayList<>();
+      for(int i = 0; i < nums.length; i++) {
+          if(lower == Integer.MAX_VALUE)
+              return result;
+          if(nums[i] < next)
+            continue;
+          if(nums[i] == next) {
+            next++;
+            continue;
+          }
+          result.add(getRange(next, nums[i]-1));
+          if(next == Integer.MAX_VALUE)
+              break;
+          next = nums[i]+1;
+      }
+      if(next <= upper)
+          result.add(getRange(next, upper));
+      return result;
+  }
+  
+  String getRange(int n1, int n2) {
+      return n1 == n2 ? String.valueOf(n1) : String.valueOf(n1) + "=>" + String.valueOf(n2); 
+  }
+}
