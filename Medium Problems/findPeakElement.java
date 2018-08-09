7d9809c70632d2cadb1ed1b727055366fe19336a
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-08-09 23:24:49 +0530

    Update findPeakElement.java

--- Medium Problems/findPeakElement.java
+++ Medium Problems/findPeakElement.java
@@ -1,14 +1,14 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int low = 0;
-        int high = nums.length-1;
+        int high = nums.length - 1;
         
         while(low < high) {
-            int mid = low + (high-low) / 2;
-            if(mid > 0 && nums[mid-1] > nums[mid])
+            int mid = low + (high - low) / 2;
+            if(mid > 0 && nums[mid - 1] > nums[mid])
                 high = mid - 1;
-            else if(mid < nums.length && nums[mid+1] > nums[mid]) {
-                low = mid+1;
+            else if(mid < nums.length && nums[mid + 1] > nums[mid]) {
+                low = mid + 1;
             }
             else
                 return mid;
