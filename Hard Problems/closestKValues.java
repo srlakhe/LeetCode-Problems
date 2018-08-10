98f5f139e7eacb998e0090f5da55f7a05cee7076
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-08-10 21:38:21 +0530

    Update closestKValues.java

--- Hard Problems/closestKValues.java
+++ Hard Problems/closestKValues.java
@@ -1,9 +1,62 @@
 import java.util.*;
 public class Solution {
     
+     // Method 1 using getNextPred and getNextSucc Methods
+     public List<Integer> closestKValues(TreeNode root, int target) {
+        Stack<TreeNode> pred = new Stack<>();
+        Stack<TreeNode> succ = new Stack<>();
+        List<Integer> res = new ArrayList<>();    
+        TreeNode itr = root;
+        while(itr != null) {
+            if(itr.val <= target) {
+                pred.push(itr);
+                itr = itr.right;
+            } else {
+                succ.push(itr);
+                itr = itr.left;
+            }
+        }
+        
+        while(k != 0) {
+            if(succ.isEmpty() && pred.isEmpty())
+                break;
+            else if(pred.isEmpty()) {
+                res.add(getNextSucc(succ));
+            } else if(succ.isEmpty()) {
+                res.add(getNextPred(pred));
+            } else if(Math.abs(target-pred.peek()) <= Math.abs(target-succ.peek()) {
+                res.add(getNextPred(pred));
+            } else
+                res.add(getNextSucc(succ));
+            k--;
+        }
+       
+        return res;
+    }
+    
+    int getNextPred(Stack<TreeNode> pred) {
+        int prev = pred.pop();
+        TreeNode itr = prev.left;
+        while(itr != null) {
+            stack.push(itr);
+            itr = itr.right;
+        }
+        return prev.val;
+    }
+    
+    int getNextSucc(Stack<TreeNode> succ) {
+        int prev = succ.pop();
+        TreeNode itr = prev.right;
+        while(itr != null) {
+            stack.push(itr);
+            itr = itr.left;
+        }
+    }
+    
+    // Method 2 using two stacks and inorder traversal
     public List<Integer> closestKValues(TreeNode root, int target) {
         Stack<TreeNode> pred = new Stack<>();
-        Stak<TreeNode> succ = new Stack<>();
+        Stack<TreeNode> succ = new Stack<>();
         List<Integer> res = new ArrayList<>();
         inorder(root, pred, succ, target);
         
