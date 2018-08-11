74680390df36416027a0947c12b64cbe4db5d683
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-08-11 23:09:15 +0530

    Add and Search Data Structure Design - Leetcode #211

---
+++ Medium Problems/addAndSearchDS.java
@@ -0,0 +1,62 @@
+class WordDictionary {
+    class TrieNode {
+            TrieNode[] arr;
+            boolean isEnd;
+            public TrieNode() {
+                arr = new TrieNode[26];
+            }
+    }
+
+    TrieNode root;
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        TrieNode p = root;
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if(p.arr[c-'a'] == null)
+                p.arr[c-'a'] = new TrieNode();
+            p = p.arr[c-'a'];
+        }
+        p.isEnd = true;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return match(root, word, 0);
+    }
+    
+    public boolean match(TrieNode root, String word, int start) {
+        if(start == word.length() && root.isEnd)
+            return true;
+        if(start >= word.length())
+            return false;
+        char c = word.charAt(start);
+        if(c == '.') {
+            for(int i = 0; i < 26; i++) {
+                if(root.arr[i] != null) {
+                    if(match(root.arr[i], word, start+1))
+                        return true;
+                }
+            }
+            return false;
+        } else {
+            if(root.arr[c-'a'] != null) {
+                return match(root.arr[c-'a'], word, start+1);
+            } else
+                return false;
+        }
+    }
+    
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
