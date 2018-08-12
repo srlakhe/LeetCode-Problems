0d659831d8b51a0eab1d0a2e374ab2a0683b177c
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-08-12 12:54:26 +0530

    Update nextGreaterElementsII.java

--- Medium Problems/nextGreaterElementsII.java
+++ Medium Problems/nextGreaterElementsII.java
@@ -1,4 +1,5 @@
 class Solution {
+    // method1: by iterating over the same array twice
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
@@ -11,6 +12,26 @@ class Solution {
             }
             if(i < n)
                 stack.push(i);
+        }
+        return res;
+    }
+    
+    // method2: by initially storing the indices on the stack
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        for(int i = n-1; i >= 0; i--)
+            stack.push(i);
+        
+        for(int i = n-1; i >= 0; i--) {
+            while(!stack.isEmpty() && nums[stack.peek()]<=nums[i])
+                stack.pop();
+            if(!stack.isEmpty()) {
+                res[i] = nums[stack.peek()];
+            }
+            stack.push(i);
         }
         return res;
     }
