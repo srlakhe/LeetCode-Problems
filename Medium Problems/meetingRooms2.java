3269bb58e4274a8607546a6d880b4f903a984125
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-08-13 19:28:57 +0530

    Meeting Rooms Ii - Leetcode #253

--- Medium Problems/meetingRooms2.java
+++ Medium Problems/meetingRooms2.java
@@ -2,22 +2,15 @@ public int minMeetingRooms(Interval[] intervals) {
     if(intervals == null|| intervals.length == 0)
         return 0;
 
-    Arrays.sort(intervals, new Comparator<Interval>() {
-        public int compare(Interval a, Interval b) {
-            return a.start - b.start;
-        }
-    });
+    Arrays.sort(intervals, (a, b) -> a.start - b.start);
     
     PriorityQueue<Integer> pq = new PriorityQueue<>();
     
-    int count = 1;
-    int mincount = 1;
     pq.add(intervals[0].end);
     for(int i = 1; i < intervals.length; i++) {
           if(intervals[i].start >= pq.peek())
               pq.poll();
-              
-           pq.offer(intervals[i].end);
+          pq.offer(intervals[i].end);
     }
     return pq.size();
 }
