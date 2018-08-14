9649189d82d08008e637945a2e0727537ddae88c
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-08-14 11:35:26 +0530

    Merge Intervals - Leetcode #56

--- Medium Problems/mergeIntervals.java
+++ Medium Problems/mergeIntervals.java
@@ -8,6 +8,7 @@
  * }
  */
 class Solution {
+    // method 1
     public List<Interval> merge(List<Interval> intervals) {
         Collections.sort(intervals, new Comparator<Interval>() {
             public int compare(Interval a, Interval b) {
@@ -33,5 +34,29 @@ class Solution {
         
         result.add(new Interval(start, end));
         return result;
+    }
+       
+     // method 2
+     public List<Interval> merge(List<Interval> intervals) {
+        int n = intervals.size();
+        int[] start = new int[n];
+        int[] end = new int[n];
+        for(int i = 0; i < n; i++) {
+            Interval t = intervals.get(i);
+            start[i] = t.start;
+            end[i] = t.end;
+        }
+        
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int j = 0;
+        List<Interval> res = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            if(i == n-1 || start[i+1] > end[i]) {
+                res.add(new Interval(start[j], end[i]));
+                j = i + 1;
+            }
+        }
+        return res;
     }
 }
