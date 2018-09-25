a59ecb19edf78f6382786a28b7e342acad006b4d
Shreyas Lakhe <srlakhe@users.noreply.github.com>
2018-09-25 12:02:53 -0700

    Solution to Rectangle Area - Leetcode #223

---
+++ Medium Problems/rectangleArea.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int area1 = (C-A) * (D-B);
+        int area2 = (G-E) * (H-F);
+        int left = Math.max(A, E);
+        int right = Math.min(C, G);
+        int top = Math.min(D, H);
+        int bottom = Math.max(B, F);
+        int res = area1 + area2;
+        if(left < right && top > bottom)
+            res -= (right-left) * (top-bottom);
+        return res;
+        
+    }
+}
